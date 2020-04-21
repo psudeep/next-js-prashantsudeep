1e99c7f7c9b7745593c6d7502f60c9a0fdd70dea
Prashant Sudeep <prashant.sudeep89@gmail.com>
2020-04-21 15:58:25 +0530

    basic working home page is live on next js

---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import { SkipNavContent } from '@reach/skip-nav';
+import { useAmp } from 'next/amp';
+
+import Page from '../components/page';
+import Footer from '../components/footer';
+
+import { ORG_NAME } from '../lib/constants';
+
+
+export default () => {
+  const isAmp = useAmp();
+
+  return (
+    <Page title={`Next.js by ${ORG_NAME} - The React Framework`}>
+{/*      <SocialMeta
+        image="/static/twitter-cards/home.jpg"
+        title={`Next.js by ${ORG_NAME} - The React Framework`}
+        url="https://nextjs.org"
+        description={`Production grade React applications that scale. The world’s leading companies use Next.js by ${ORG_NAME} to build static and dynamic websites and web applications.`}
+      />*/}
+      <SkipNavContent />
+      {/*<Notification href="/blog/next-9-3" title="Next 9.3 is out!" titleMobile="Next 9.3 is out!">
+        <b>Next 9.3 is out!</b> — Next-gen SSG Support, Preview Mode, Sass Support, and more!
+        <span className="highlight">Learn More →</span>
+      </Notification>*/}
+      {/*<Intro isAmp={isAmp} />
+      <Demo />
+      <Features />
+      <Customers />
+      <Learn />
+      <Newsletter />*/}
+      <Footer />
+    </Page>
+  );
+};
+
+export const config = {
+  amp: 'hybrid'
+};
